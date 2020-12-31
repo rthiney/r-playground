d622abc13553ff0938516912d11255db4a763472
Raphael Thiney <raphael.thiney@gmail.com>
2020-12-30 18:18:48 -0700

    more git

--- playground.R
+++ playground.R
@@ -1,9 +1,18 @@
- # Load the gapminder package
+# Load the gapminder package
 library(gapminder)
-
 # Load the dplyr package
 library(dplyr)
 
+library(ggplot2)
 # Look at the gapminder dataset
-gapminder %>% 
- filter(year == 2007)
+gapminder_2007 <- gapminder %>%
+    filter(year == 2007) %>%
+    mutate(lifeExpMonths = lifeExp * 12) %>%
+    arrange(desc(lifeExpMonths))
+
+
+print(gapminder_2007)
+
+ggplot(gapminder_2007, aes(x = gdpPercap, y = lifeExp)) +
+    geom_point() +
+    scale_x_log10()
